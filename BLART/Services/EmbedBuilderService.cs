c9bebff25de89e098dd6ec5c7dc29f449549f3bb
joker-119 <amathor929@gmail.com>
2022-04-29 19:47:14 -0700

    Version stuff

--- BLART/Services/EmbedBuilderService.cs
+++ BLART/Services/EmbedBuilderService.cs
@@ -1,10 +1,11 @@
 namespace BLART.Services;
 
+using System.Reflection;
 using Discord;
 
 public class EmbedBuilderService
 {
-    public static string Footer => "Benevolent Lawmaking, Analysis and Reinforcement Technology - Joker119";
+    public static string Footer => $"Benevolent Lawmaking, Analysis and Reinforcement Technology | {Assembly.GetExecutingAssembly().GetName().Version} | - Joker119";
     
     public static async Task<Embed> CreateBasicEmbed(string title, string description, Color color) => await Task.Run(
         () => new EmbedBuilder().WithTitle(title).WithDescription(description).WithColor(color).WithCurrentTimestamp()
