33f474bcb89d47d5bc6c6f9f440c483d2e7dfe75
Thunder <thundermaker300@gmail.com>
2023-09-02 18:26:28 -0400

    Remove existing

--- BLART/Commands/StickiedMessages/RemoveMessage.cs
+++ BLART/Commands/StickiedMessages/RemoveMessage.cs
@@ -22,6 +22,18 @@ public partial class StickiedMessages : InteractionModuleBase<SocketInteractionC
             return;
         }
 
+        // Remove existing message
+        string? id = DatabaseHandler.GetStickyMessageID(channel.Id);
+        if (id is not null)
+        {
+            await DeferAsync(true);
+
+            IMessage msg = await channel.GetMessageAsync(ulong.Parse(id));
+
+            if (msg is not null)
+                await msg.DeleteAsync(new() { AuditLogReason = "Removed message for deleted sticky message." });
+        }
+
         DatabaseHandler.RemoveEntry(channel.Id, DatabaseType.StickiedMessage);
         await RespondAsync($"Sticky message has been removed from the {channel.Mention} channel!", ephemeral: true);
     }
