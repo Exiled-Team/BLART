87219562d0277a8ff870862d96e70b54f5372892
xRoier <rogerazomar@gmail.com>
2023-10-20 02:49:37 +0200

    Delete Config file

--- BLART/Program.cs
+++ BLART/Program.cs
@@ -6,28 +6,16 @@ using Newtonsoft.Json;
 
 public static class Program
 {
-    private static Config? _config;
-    private static string KCfgFile = "Blart.json";
     private static Bot? _bot;
 
     public static string DatabaseFile { get; } = Path.Combine(Environment.CurrentDirectory, "Blart.db");
-    public static Config Config => _config ??= GetConfig();
+    public static Config Config => Config.Default;
     public static Random Rng { get; } = new();
 
     public static void Main(string[] args)
     {
         Console.WriteLine($"Starting. Version: {Assembly.GetExecutingAssembly().GetName().Version}");
-        if (args.Contains("--debug"))
-            Config.Debug = true;
         _bot = new Bot(args);
         AppDomain.CurrentDomain.ProcessExit += (_, _) => _bot.Destroy();
-    }
-
-    private static Config GetConfig()
-    {
-        if (File.Exists(KCfgFile))
-            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(KCfgFile))!;
-        File.WriteAllText(KCfgFile, JsonConvert.SerializeObject(Config.Default, Formatting.Indented));
-        return Config.Default;
     }
 }
